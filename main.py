134bc58690b68dac86ccad657776bf049a35fc0c
NoahL <nrleuthold@gmail.com>
2021-12-15 10:36:50 -0600

    Players finished (colors and sizes)

--- main.py
+++ main.py
@@ -9,10 +9,25 @@ pygame.display.set_caption("Tic-Tac-Toe")
 
 BG = pygame.transform.scale(pygame.image.load(os.path.join("Board.png")).convert_alpha(), (WIDTH, HEIGHT))
 
+PlayX = pygame.image.load("X.png").convert_alpha()
+PlayX = pygame.transform.scale(PlayX, (20, 20))
+
+PlayO = pygame.image.load("O.png").convert_alpha()
+PlayO = pygame.transform.scale(PlayO, (20, 20))
+
+FinX = pygame.image.load("X.png").convert_alpha()
+FinX = pygame.transform.scale(FinX, (125, 125))
+
+FinO = pygame.image.load("O.png").convert_alpha()
+FinO = pygame.transform.scale(FinO, (125, 125))
 
 while True:
 
     WIN.blit(BG, (0,0))
+    WIN.blit(PlayX, (50, 50))
+    WIN.blit(PlayO, (50, 90))
+    WIN.blit(FinX, (38, 189))
+    WIN.blit(FinO, (38, 338))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
